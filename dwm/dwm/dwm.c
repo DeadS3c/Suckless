7ae2d6bbfee06fd2fffa2e4c7b60a081ae9c8415
DeadS3c <h4ck0rd@gmail.com>
2022-01-12 16:00:19 +0100

    Another one bite the dust...

--- dwm/dwm/dwm.c
+++ dwm/dwm/dwm.c
@@ -60,7 +60,6 @@
                                * MAX(0, MIN((y)+(h),(m)->wy+(m)->wh) - MAX((y),(m)->wy)))
 #define ISVISIBLE(C)            ((C->tags & C->mon->tagset[C->mon->seltags]) || C->issticky)
 #define ISINC(X)                ((X) > 1000 && (X) < 3000)
-#define ISVISIBLE(C)            ((C->tags & C->mon->tagset[C->mon->seltags]))
 #define PREVSEL                 3000
 #define LENGTH(X)               (sizeof X / sizeof X[0])
 #define MOD(N,M)                ((N)%(M) < 0 ? (N)%(M) + (M) : (N)%(M))
